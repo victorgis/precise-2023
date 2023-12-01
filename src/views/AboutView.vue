de73a15351198d31a1dca6fe9f1acbd4495933e6
victorgis <essangvictor@gmail.com>
2023-12-02 00:11:30 +0100

    added doanload lines

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,30 +2,43 @@
   <div class="about">
     <div>
       <h1>This is an about page</h1>
-      <!-- Adjust the file path based on your project structure -->
-      <a class="niching-btn"
-        href="../downloads/locate-share.apk"
-        download="locate-share.apk"
-      >
-        Download now
-      </a>
+      <!-- Add a button with a click event handler -->
+      <button @click="downloadFile" class="niching-btn">Download now</button>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    downloadFile() {
+      // Specify the filename and path (adjust based on your project structure)
+      const filePath = "../downloads/locate-share.apk";
+
+      // Create a link element
+      const link = document.createElement("a");
+      link.href = filePath;
+      link.download = "locate-share.apk";
+
+      // Append the link to the document
+      document.body.appendChild(link);
+
+      // Trigger a click event on the link
+      link.click();
+
+      // Remove the link from the document
+      document.body.removeChild(link);
+    },
+  },
+};
+</script>
+
 <style>
-/* Uncomment and modify styles if needed */
-/*@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}*/
 .niching-btn {
   color: #262626;
   background: #fff;
   border: #262626 1px solid;
   padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
