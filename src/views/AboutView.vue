78cdbb95178dda84cd416fa51c6fe922f884f4bf
victorgis <essangvictor@gmail.com>
2023-12-01 23:58:38 +0100

    added doanload lines

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="about">
-    
-    <div class="">
+    <div>
       <h1>This is an about page</h1>
+      <!-- Adjust the file path based on your project structure -->
       <a class="niching-btn"
         href="../downloads/locate-share.apk"
-        download="../downloads/locate-share.apk"
-        >Download now</a
+        download="locate-share.apk"
       >
+        Download now
+      </a>
     </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  /* .about {
+/* Uncomment and modify styles if needed */
+/*@media (min-width: 1024px) {
+  .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
-  } */
-}
+  }
+}*/
 .niching-btn {
   color: #262626;
   background: #fff;
